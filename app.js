3a77d433a452fb758950e1e480e59e730094145e
Satyam Srivastava <satyams.cs20@nsut.ac.in>
2021-05-04 00:21:33 +0530

    had an error process P was caps

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.post("/failure",function(req,res){
 
   res.redirect("/");
 })
-app.listen( Process.env.PORT || 3000,function(){
+app.listen(process.env.PORT || 3000,function(){
 
   console.log("server is up");
 });
